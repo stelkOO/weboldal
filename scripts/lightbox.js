aa0900a76142f043daee4370ade8d6fe21027954
stelkOO <156014089+stelkOO@users.noreply.github.com>
2024-03-13 20:14:36 +0100

    Add files via upload

---
+++ scripts/lightbox.js
@@ -0,0 +1,24 @@
+// FORRÁS //
+// https://github.com/WebDevSimplified/Lightbox/blob/master/script.js //
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})

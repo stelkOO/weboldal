5aac4dca7c0edd8a76e567379110546d79bfd99f
stelkOO <156014089+stelkOO@users.noreply.github.com>
2024-03-16 04:06:14 +0100

    Update lightbox.js

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,24 +1,22 @@
-// FORRÁS //
-// https://github.com/WebDevSimplified/Lightbox/blob/master/script.js //
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
 
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+const images = document.querySelectorAll('img:not(#ne)');
 
-const images = document.querySelectorAll('img')
 images.forEach(image => {
   image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.src = image.src;
     while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
+      lightbox.removeChild(lightbox.firstChild);
     }
-    lightbox.appendChild(img)
-  })
-})
+    lightbox.appendChild(img);
+  });
+});
 
 lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove('active');
+});
